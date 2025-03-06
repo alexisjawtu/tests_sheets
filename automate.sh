1db6ddaf80c0485b9ae053eca3de0770cf55102a
Alexis Jawtuschenko <alexis.jawtuschenkogmail.com>
2025-03-06 20:19:50 -0300

    WIP build input and output sets

--- automate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-# diff elements.dat ../tests_wafers/inner/2/elements.dat 
-# diff bdr_nodes.dat ../tests_wafers/inner/2/bdr_nodes.dat 
-# diff nodes.dat ../tests_wafers/inner/2/nodes.dat 
-
-mkdir inner/7/result
-cylinder 
---
+++ inner/craftinputs.sh
@@ -0,0 +1,146 @@
+#!/usr/bin/bash
+
+# crafting a collection of inputs
+
+echo "folder_missing: ./," >> 09/input.json
+echo "wafer_type: 2," >> 09/input.json
+echo "user_diameter_for_wafer: 598," >> 09/input.json
+echo "user_thickness_of_inner_wafer: 797," >> 09/input.json
+echo "mesh_scale: 6" >> 09/input.json
+echo "sensor_x: 191," >> 09/input.json
+echo "sensor_y: 171," >> 09/input.json
+echo "probe_angle: 27," >> 09/input.json
+echo "pitch: 4.9," >> 09/input.json
+echo "len_orient_flat_1: 68," >> 09/input.json
+
+echo "test_name: Ex10_Type2," > 10/input.json
+echo "folder_missing: ./," >> 10/input.json
+echo "wafer_type: 2," >> 10/input.json
+echo "user_diameter_for_wafer: 597," >> 10/input.json
+echo "user_thickness_of_inner_wafer: 798," >> 10/input.json
+echo "mesh_scale: 7" >> 10/input.json
+echo "sensor_x: 190," >> 10/input.json
+echo "sensor_y: -170," >> 10/input.json
+echo "probe_angle: 28," >> 10/input.json
+echo "pitch: 4.8," >> 10/input.json
+echo "len_orient_flat_1: 67," >> 10/input.json
+
+echo "test_name: Ex11_Type2," > 11/input.json
+echo "folder_missing: ./," >> 11/input.json
+echo "wafer_type: 2," >> 11/input.json
+echo "user_diameter_for_wafer: 596," >> 11/input.json
+echo "user_thickness_of_inner_wafer: 799," >> 11/input.json
+echo "mesh_scale: 11.1" >> 11/input.json
+echo "sensor_x: 189," >> 11/input.json
+echo "sensor_y: -169," >> 11/input.json
+echo "probe_angle: 29," >> 11/input.json
+echo "pitch: 4.7," >> 11/input.json
+echo "len_orient_flat_1: 66," >> 11/input.json
+
+echo "test_name: Ex12_Type2," > 12/input.json
+echo "folder_missing: ./," >> 12/input.json
+echo "wafer_type: 2," >> 12/input.json
+echo "user_diameter_for_wafer: 595," >> 12/input.json
+echo "user_thickness_of_inner_wafer: 800," >> 12/input.json
+echo "mesh_scale: 11.2" >> 12/input.json
+echo "sensor_x: 188," >> 12/input.json
+echo "sensor_y: -168," >> 12/input.json
+echo "probe_angle: 30," >> 12/input.json
+echo "pitch: 4.6," >> 12/input.json
+echo "len_orient_flat_1: 65," >> 12/input.json
+
+echo "test_name: Ex13_Type2," > 13/input.json
+echo "folder_missing: ./," >> 13/input.json
+echo "wafer_type: 2," >> 13/input.json
+echo "user_diameter_for_wafer: 594," >> 13/input.json
+echo "user_thickness_of_inner_wafer: 796," >> 13/input.json
+echo "mesh_scale: 11.3" >> 13/input.json
+echo "sensor_x: 187," >> 13/input.json
+echo "sensor_y: -167," >> 13/input.json
+echo "probe_angle: 31," >> 13/input.json
+echo "pitch: 4.5," >> 13/input.json
+echo "len_orient_flat_1: 64," >> 13/input.json
+
+echo "test_name: Ex14_Type2," > 14/input.json
+echo "folder_missing: ./," >> 14/input.json
+echo "wafer_type: 2," >> 14/input.json
+echo "user_diameter_for_wafer: 593," >> 14/input.json
+echo "user_thickness_of_inner_wafer: 795," >> 14/input.json
+echo "mesh_scale: 11.4" >> 14/input.json
+echo "sensor_x: 186," >> 14/input.json
+echo "sensor_y: -166," >> 14/input.json
+echo "probe_angle: 32," >> 14/input.json
+echo "pitch: 4.4," >> 14/input.json
+echo "len_orient_flat_1: 63," >> 14/input.json
+
+echo "test_name: Ex15_Type2," > 15/input.json
+echo "folder_missing: ./," >> 15/input.json
+echo "wafer_type: 2," >> 15/input.json
+echo "user_diameter_for_wafer: 592," >> 15/input.json
+echo "user_thickness_of_inner_wafer: 794," >> 15/input.json
+echo "mesh_scale: 11.5" >> 15/input.json
+echo "sensor_x: 185," >> 15/input.json
+echo "sensor_y: -165," >> 15/input.json
+echo "probe_angle: 33," >> 15/input.json
+echo "pitch: 4.3," >> 15/input.json
+echo "len_orient_flat_1: 62," >> 15/input.json
+
+echo "test_name: Ex16_Type2," > 16/input.json
+echo "folder_missing: ./," >> 16/input.json
+echo "wafer_type: 2," >> 16/input.json
+echo "user_diameter_for_wafer: 591," >> 16/input.json
+echo "user_thickness_of_inner_wafer: 690," >> 16/input.json
+echo "mesh_scale: 11.6" >> 16/input.json
+echo "sensor_x: 184," >> 16/input.json
+echo "sensor_y: -164," >> 16/input.json
+echo "probe_angle: 34," >> 16/input.json
+echo "pitch: 4.2," >> 16/input.json
+echo "len_orient_flat_1: 61," >> 16/input.json
+
+echo "test_name: Ex17_Type2," > 17/input.json
+echo "folder_missing: ./," >> 17/input.json
+echo "wafer_type: 2," >> 17/input.json
+echo "user_diameter_for_wafer: 590," >> 17/input.json
+echo "user_thickness_of_inner_wafer: 601," >> 17/input.json
+echo "mesh_scale: 11.7" >> 17/input.json
+echo "sensor_x: 183," >> 17/input.json
+echo "sensor_y: -163," >> 17/input.json
+echo "probe_angle: 35," >> 17/input.json
+echo "pitch: 4.1," >> 17/input.json
+echo "len_orient_flat_1: 60," >> 17/input.json
+
+echo "test_name: Ex18_Type2," > 18/input.json
+echo "folder_missing: ./," >> 18/input.json
+echo "wafer_type: 2," >> 18/input.json
+echo "user_diameter_for_wafer: 589," >> 18/input.json
+echo "user_thickness_of_inner_wafer: 599," >> 18/input.json
+echo "mesh_scale: 11.8" >> 18/input.json
+echo "sensor_x: 182," >> 18/input.json
+echo "sensor_y: -162," >> 18/input.json
+echo "probe_angle: 36," >> 18/input.json
+echo "pitch: 4," >> 18/input.json
+echo "len_orient_flat_1: 59," >> 18/input.json
+
+echo "test_name: Ex19_Type2," > 19/input.json
+echo "folder_missing: ./," >> 19/input.json
+echo "wafer_type: 2," >> 19/input.json
+echo "user_diameter_for_wafer: 588," >> 19/input.json
+echo "user_thickness_of_inner_wafer: 598," >> 19/input.json
+echo "mesh_scale: 11.9" >> 19/input.json
+echo "sensor_x: 181," >> 19/input.json
+echo "sensor_y: -161," >> 19/input.json
+echo "probe_angle: 37," >> 19/input.json
+echo "pitch: 3.9," >> 19/input.json
+echo "len_orient_flat_1: 58," >> 19/input.json
+
+echo "test_name: Ex20_Type2," > 20/input.json
+echo "folder_missing: ./," >> 20/input.json
+echo "wafer_type: 2," >> 20/input.json
+echo "user_diameter_for_wafer: 587," >> 20/input.json
+echo "user_thickness_of_inner_wafer: 597," >> 20/input.json
+echo "mesh_scale: 9.8" >> 20/input.json
+echo "sensor_x: 180," >> 20/input.json
+echo "sensor_y: -160," >> 20/input.json
+echo "probe_angle: 38," >> 20/input.json
+echo "pitch: 3.8," >> 20/input.json
+echo "len_orient_flat_1: 57," >> 20/input.json
