c30e5835ce0016ba20455653d26226e629ea87cc
alexis <alexis.jawtuschenko@gmail.com>
2025-04-18 20:04:49 -0300

    WIP

---
+++ common.h
@@ -0,0 +1,9 @@
+namespace magicliterals
+{
+    constexpr char pathout[] {"/home/alexis/code/tests_sheets/cylinder/tmp/"};
+    constexpr char vertstable[] {"-cylinder_verts_by_elems.dat"};
+    constexpr char bdry2Dverts[] {"-2D_bdr_vertices.dat"};
+    constexpr char elemstable[] {"-cylinder_elements.dat"};
+    constexpr char vertices3D[] {"-cylinder_vertices.dat"};
+    constexpr int prcsn {16};
+}
