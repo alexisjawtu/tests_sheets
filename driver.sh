d000cb9930e081bd7d073218631ba089aa74b7e3
alexis <alexis.jawtuschenko@gmail.com>
2025-04-25 14:33:29 -0300

    WIP

--- driver.sh
+++ driver.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/bash
 
+fname=run_cases
+std="c++20"
+
+echo "std=$std"
+c++ -std=$std $fname.cpp -I . -o $fname.o
+compresult=$?
+echo "Compilation of $fname finished with return status $compresult"
+if [ $compresult -ne 0 ]; then
+    exit
+fi
+
+compresult=$(make_cyl)
+echo $compresult
+
+./$fname.o
+
 ls cylinder/tmp/*2D* -1 > ./.tmpfiles.txt
 cp ./.tmpfiles.txt ./.files.txt
 sed -i s/'tmp\/'//g ./.files.txt
